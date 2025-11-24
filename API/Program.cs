a6251a2c256a8ae75687d659b30fddf425b14b79
Umar Farooq <136256966+umarfarooqw99@users.noreply.github.com>
2025-11-25 00:25:34 +0500

    End of section 5

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 //ordering doesn't matter for services, but for middleware it does
 
+using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -14,6 +16,8 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 });
 
 builder.Services.AddCors();
+builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivitiesList>());
+builder.Services.AddAutoMapper(typeof (MappingProfiles).Assembly);
 
 var app = builder.Build();
 
